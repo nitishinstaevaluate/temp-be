a4ca344abbded82812f46f14fef1650cbbc110a9
Sunil Kumar <kumarsunil1977188@gmail.com>
2023-03-29 17:53:04 +0530

    Updated

--- src/valuationProcess/calculationMethods.ts
+++ src/valuationProcess/calculationMethods.ts
@@ -3,11 +3,27 @@ export function FCFEMethod(inputs:object) {
   //Perform calculations here.
 
   const result={
-    "valuation":87898797,
-    "presentValue": 7878787,
+    "particulars":2023,
+    "pat":324323,
+    "depAndAmortisation":797987,
+    "onCashItems":324234,
+    "nca":88797,
+    "defferedTaxAssets":87979,
+    "netCashFlow":7779,
+    "fixedAssets":877879, 
+    "fcff":87898797,
+    "discountingPeriod":7667,
+    "discountingFactor":9887,
+    "presentFCFF":7898,
+    "sumOfCashFlows":9789798,
+    "debtOnDate":87979,
+    "ccEquivalents":787987,
+    "surplusAssets":89890,
+    "otherAdj":78979,
     "equityValue": 667676,
+    "noOfShares":898789,
     "valuePerShare":4534.34
-  }
+      };
   return {"message":"Calculated valuation using FCFE",result:result};
 }
 
