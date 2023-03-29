42fd66b9a1428f411ce5881f95862c65e148ed60
Sunil Kumar <kumarsunil1977188@gmail.com>
2023-03-29 18:32:30 +0530

    Updated

--- src/valuationProcess/valuationProcess.controller.ts
+++ src/valuationProcess/valuationProcess.controller.ts
@@ -11,16 +11,16 @@ export class ValuationController {
   @Post()
  async processExcelFile (@Body() inputs): Promise<any> {
     
-const workbook = XLSX.readFile('./uploads/FCFE_Template.xlsx');
+  //get all input values which we needs in calculation from sheet1 and sheet2.
+ console.log('Inputs',inputs);
+ const {model,company,userId,excelSheetId}=inputs;
+
+const workbook = XLSX.readFile(`./uploads/${excelSheetId}`);
 const worksheet1 = workbook.Sheets['P&L'];
 const sheet1 = XLSX.utils.sheet_to_json(worksheet1);
 const worksheet2 = workbook.Sheets['BS'];
 const sheet2 = XLSX.utils.sheet_to_json(worksheet2);
 // return {"sheet1":sheet1,"sheet2":sheet2};
-
-//get all input values which we needs in calculation from sheet1 and sheet2.
- console.log('Inputs',inputs);
-const {model,company,userId}=inputs;
 
 // Performe calculation by specific method
 if(model==="FCFE"){
