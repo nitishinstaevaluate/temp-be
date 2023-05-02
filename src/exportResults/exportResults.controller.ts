49d79469f3a8294c55f562c5f7d9c5505ada8cf1
Sunil Kumar <kumarsunil1977188@gmail.com>
2023-05-02 10:00:19 +0530

    user info added in pdf export header.

--- src/exportResults/exportResults.controller.ts
+++ src/exportResults/exportResults.controller.ts
@@ -3,17 +3,21 @@ import { Response } from 'express';
 import { generatePdf } from './exportResults.method';
 import { ValuationsService } from 'src/valuationProcess/valuationProcess.service';
 import { CompaniesService } from 'src/masters/masters.service';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('export')
 export class ExportResultsController {
   constructor(
     private valuationsService: ValuationsService,
     private companiesService: CompaniesService,
+    private usersService:UsersService
   ) {}
   @Get(':reportId')
   async generatePdf(@Param('reportId') reportId: string, @Res() res: Response) {
     const valuation = await this.valuationsService.getValuationById(reportId);
     if (valuation) {
+             const user=await this.usersService.getUserById(valuation.userId);
+             valuation["user"]=user;
       if (valuation.model === 'Relative_Valuation') {
         const promises = valuation.inputData.companies.map((id) =>
           this.companiesService.getCompanyById(id),
--- src/exportResults/exportResults.module.ts
+++ src/exportResults/exportResults.module.ts
@@ -3,8 +3,9 @@ import { ExportResultsController } from './exportResults.controller';
 import { ValuationProcessModule } from 'src/valuationProcess/valuationProcess.module';
 import { LoggerModule } from 'src/loggerService/logger.module';
 import { MastersModule } from 'src/masters/masters.module';
+import { UsersModule } from 'src/users/users.module';
 @Module({
-  imports: [ValuationProcessModule,LoggerModule,MastersModule],
+  imports: [ValuationProcessModule,LoggerModule,MastersModule,UsersModule],
   controllers: [ExportResultsController], //ImportController
   providers: [], //ImportService
 })
