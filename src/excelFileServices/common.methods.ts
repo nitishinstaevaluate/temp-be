e1d9b5ab9b6ed835aa42a7e9ba41547302b9c1bc
Sunil Kumar <kumarsunil1977188@gmail.com>
2023-05-22 11:50:39 +0530

    Updated

--- src/excelFileServices/common.methods.ts
+++ src/excelFileServices/common.methods.ts
@@ -5,33 +5,43 @@ export async function getCellValue(worksheet: any, address: string) {
   return value;
 }
 
+export function findMedian(numbers: number[]) {
+  numbers.sort((a, b) => a - b);
+  const middleIndex = Math.floor(numbers.length / 2);
+  const isEvenLength = numbers.length % 2 === 0;
+  if (isEvenLength) {
+    return (numbers[middleIndex - 1] + numbers[middleIndex]) / 2;
+  } else {
+    return numbers[middleIndex];
+  }
+}
 
-export function findMedian(numbers:number[]) {
-    numbers.sort((a, b) => a - b);
-    const middleIndex = Math.floor(numbers.length / 2);
-    const isEvenLength = numbers.length % 2 === 0;
-    if (isEvenLength) {
-      return (numbers[middleIndex - 1] + numbers[middleIndex]) / 2;
-    } else {
-      return numbers[middleIndex];
-    }
-  }
-  
- export function findAverage(numbers:number[]) {
-    const sum = numbers.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0,
-    );
-    const average = sum / numbers.length;
-    return average;
-  }
+export function findAverage(numbers: number[]) {
+  const sum = numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0,
+  );
+  const average = sum / numbers.length;
+  return average;
+}
 
-  export function calculateDaysFromDate(dateString:string) {
-    const date = new Date(dateString);
-    const startDate = new Date(`${date.getFullYear()}-04-01`); // April 1st
-    const endDate =date;
-    const timeDiff = endDate.getTime() - startDate.getTime();
-    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24)) + 1;
-    
-    return days;
-  }
+export function calculateDaysFromDate(dateString: string) {
+  const date = new Date(dateString);
+  const startDate = new Date(`${date.getFullYear()}-04-01`); // April 1st
+  const endDate = date;
+  const timeDiff = endDate.getTime() - startDate.getTime();
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24)) + 1;
+  const totalDays = isLeapYear(date.getFullYear()) ? 366 : 365;
+  if (days <= 0){
+    const diff=totalDays - -days;
+  if(diff===0)
+  return totalDays;
+  else
+  return diff;
+  } 
+  return days;
+}
+
+export function isLeapYear(year: number) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
--- src/valuationProcess/valuationProcess.controller.ts
+++ src/valuationProcess/valuationProcess.controller.ts
@@ -10,7 +10,10 @@ import * as XLSX from 'xlsx';
 import { ValuationsService } from './valuationProcess.service';
 import { ValuationMethodsService } from './valuation.methods.service';
 import { MyMiddleware } from '../middleware/Valuation.middleware';
-import { calculateDaysFromDate } from '../excelFileServices/common.methods';
+import {
+  calculateDaysFromDate,
+  isLeapYear,
+} from '../excelFileServices/common.methods';
 @Controller('valuationProcess')
 @UseInterceptors(MyMiddleware)
 export class ValuationProcessController {
@@ -21,40 +24,49 @@ export class ValuationProcessController {
 
   @Post()
   async processExcelFile(@Body() inputs): Promise<any> {
-    const { model,valuationDate, company, userId, excelSheetId } = inputs;
+    const { model, valuationDate, company, userId, excelSheetId } = inputs;
 
     const workbook = XLSX.readFile(`./uploads/${excelSheetId}`);
     const worksheet1 = workbook.Sheets['P&L'];
     const worksheet2 = workbook.Sheets['BS'];
-    
-    if (model === 'FCFE'||model==="FCFF") {
-    const plDays = calculateDaysFromDate(valuationDate);
-    console.log('Testing....................','Date: ',valuationDate,"Days:", plDays);
-    // Change column B values for worksheet1
-    const range = XLSX.utils.decode_range(worksheet1['!ref']);
 
-    for (let rowNum = range.s.r + 1; rowNum <= range.e.r; rowNum++) {
-      const cellAddress = XLSX.utils.encode_cell({ r: rowNum, c: 1 }); // Column B
-      const cell = worksheet1[cellAddress];
-      if (cell && cell.t === 'n') {
-        // Check if the cell contains a number
-        cell.v = (cell.v / plDays) * (365 - plDays) + cell.v;
+    if (model === 'FCFE' || model === 'FCFF') {
+      const plDays = calculateDaysFromDate(valuationDate);
+      const date = new Date(valuationDate);
+      const totalDays = isLeapYear(date.getFullYear()) ? 366 : 365;
+      if (plDays <totalDays) {
+        console.log(
+          'Testing....................',
+          'Date: ',
+          valuationDate,
+          'Days:',
+          plDays,
+        );
+        // Change column B values for worksheet1
+        const range = XLSX.utils.decode_range(worksheet1['!ref']);
+
+        for (let rowNum = range.s.r + 1; rowNum <= range.e.r; rowNum++) {
+          const cellAddress = XLSX.utils.encode_cell({ r: rowNum, c: 1 }); // Column B
+          const cell = worksheet1[cellAddress];
+          if (cell && cell.t === 'n') {
+            // Check if the cell contains a number
+            cell.v = (cell.v / plDays) * (365 - plDays) + cell.v;
+          }
+        }
+
+        // Change column B values for worksheet2
+        const range2 = XLSX.utils.decode_range(worksheet2['!ref']);
+
+        for (let rowNum = range2.s.r + 1; rowNum <= range2.e.r; rowNum++) {
+          const cellAddress = XLSX.utils.encode_cell({ r: rowNum, c: 1 }); // Column B
+          const cell = worksheet2[cellAddress];
+          if (cell && cell.t === 'n') {
+            // Check if the cell contains a number
+            cell.v = (cell.v / plDays) * (365 - plDays) + cell.v;
+          }
+        }
       }
     }
-
-    // Change column B values for worksheet2
-    const range2 = XLSX.utils.decode_range(worksheet2['!ref']);
-
-    for (let rowNum = range2.s.r + 1; rowNum <= range2.e.r; rowNum++) {
-      const cellAddress = XLSX.utils.encode_cell({ r: rowNum, c: 1 }); // Column B
-      const cell = worksheet2[cellAddress];
-      if (cell && cell.t === 'n') {
-        // Check if the cell contains a number
-        cell.v = (cell.v / plDays) * (365 - plDays) + cell.v;
-      }
-    }
-
-  }
 
     // Performe calculation by specific method
     if (model === 'FCFE') {
@@ -70,7 +82,7 @@ export class ValuationProcessController {
       const data = {
         company: company,
         model: model,
-        valuation:valuationResponse.valuation,
+        valuation: valuationResponse.valuation,
         inputData: inputs,
         valuationData: valuationResult,
         userId: userId,
@@ -92,7 +104,7 @@ export class ValuationProcessController {
       const data = {
         company: company,
         model: model,
-        valuation:valuationResponse.valuation,
+        valuation: valuationResponse.valuation,
         inputData: inputs,
         valuationData: valuationResult,
         userId: userId,
