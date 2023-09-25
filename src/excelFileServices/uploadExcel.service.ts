f3ed11c3044c14c87cb345c5e4330c8fedf079c5
sanket-ifinworth <sanket@ifinworth.com>
2023-09-25 14:02:28 +0530

    added header and footer to pdf

--- src/excelFileServices/uploadExcel.service.ts
+++ src/excelFileServices/uploadExcel.service.ts
@@ -5,6 +5,7 @@ import * as xlsx from 'xlsx';
 import { Observable, throwError, of, from } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import * as puppeteer from 'puppeteer';
+import { mainLogo } from 'src/constants/constants';
 
 @Injectable()
 export class ExcelSheetService {
@@ -75,12 +76,51 @@ export class ExcelSheetService {
       async generatePdf(htmlContent: string, pdfFilePath: string) {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
+
+       
     
         try {
-          // Set the HTML content
           const contenread = await page.setContent(htmlContent);
           console.log(contenread,"content from html")
-          await page.pdf({ path: pdfFilePath, format: 'A4', height: '20mm'});
+            
+          
+          await page.pdf({
+            path: pdfFilePath,
+            format: 'A4' as puppeteer.PaperFormat, // Cast 'A4' to PaperFormat
+            displayHeaderFooter: true,
+            footerTemplate: `<table style="margin: 20px; width: 100%;">
+            <tr>
+              <td colspan="4" style="text-align: right; font-size: 12px; padding: 10px;">
+                <table style="width: 100%; border-top: 1px solid #000; border-bottom: 1px solid #000;">
+                  <tr>
+                    <td style="width: 15%;">&nbsp;</td>
+                    <td style="width: 70%; text-align: center;">
+                      <span style="font-size: 10px;">Ifinworth | Page <span class="pageNumber"></span> of <span class="totalPages"></span></span>
+                    </td>
+                    <td style="width: 15%; font-size: 10px;">V1/ Sept / 2023</td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>`,
+          headerTemplate: `
+          <table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+          <td style="width:86.2%;">
+          
+            <table border="0" cellspacing="0" cellpadding="0" style="height: 20px;width:100% !important;padding-left:2%;">
+              <tr>
+                <td style="border-bottom: solid 2px #03002f !important; font-size: 13px; height: 5px;width:100% !important;">Ifinworth Advisors Private Ltd.</td>
+              </tr>
+
+              <tr>
+                <td style="font-size: 11px">&nbsp;</td>
+              </tr>
+            </table>
+          </td>
+        <td  align="right" style="padding-right:1%;" height="10px"><img src="${mainLogo}" width="100" height="" /></td>
+        </tr></table>`
+          });
         } catch (error) {
           console.error('Error generating PDF:', error);
         } finally {
