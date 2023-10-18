c15b8becb05e7bdc80c58f5a13a5b7e7b1ac90d8
sanket-ifinworth <sanket@ifinworth.com>
2023-10-18 11:06:55 +0530

    fix:date format along with font transformation in index page

--- src/report/report/report.service.ts
+++ src/report/report/report.service.ts
@@ -384,8 +384,8 @@ export class ReportService {
 
     formatDate(date: Date): string {
       const months = [
-        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
       ];
   
       const day = date.getDate();
