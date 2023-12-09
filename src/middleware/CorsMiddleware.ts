48dfd9380c2016d16195f69090a2cf9581a2267c
sanket-ifinworth <sanket@ifinworth.com>
2023-12-09 12:30:51 +0530

    feature:implemented winston logger/error interceptor

--- src/middleware/CorsMiddleware.ts
+++ src/middleware/CorsMiddleware.ts
@@ -1,8 +1,29 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import {  transports, format } from 'winston';
+import { utilities as nestWinstonModuleUtilities, WinstonModule,  } from 'nest-winston';
+
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
+
+  private readonly logger = WinstonModule.createLogger({
+    transports: [
+      new transports.File({ filename: 'combined.log' }),
+      new transports.File({ filename: 'error.log', level: 'error' }),
+      new transports.Console({
+        format: format.combine(
+          format.timestamp(),
+          format.ms(),
+          nestWinstonModuleUtilities.format.nestLike('Ifin', {
+            colors: true,
+            prettyPrint: true,
+          }),
+        ),
+      }),
+    ],
+  });
+
   use(req: Request, res: Response, next: NextFunction) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -12,6 +33,17 @@ export class CorsMiddleware implements NestMiddleware {
     if (req.method === 'OPTIONS') {
       res.status(200).send();
     } else {
+      const { method, originalUrl } = req;
+
+      res.on('finish', () => {
+        const { statusCode } = res;
+        if (statusCode >= 400) {
+          this.logger.error(`Error Response ${method} ${originalUrl} ${statusCode} ${JSON.stringify({ error: Error.captureStackTrace, body: req.body })}`, );
+        } else {
+          this.logger.log(`Response ${method} ${originalUrl} ${statusCode} ${JSON.stringify({ body: req.body })}`);
+        }
+      });
+
       next();
     }
   }
